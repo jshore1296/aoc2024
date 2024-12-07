9789e656f9a22efcc617f3a91e26ff170b49be3b
Jean-Claude Shore <jshore1296@gmail.com>
2024-12-07 14:44:14 -0500

    finish day 6

--- 06/main.go
+++ 06/main.go
@@ -7,9 +7,12 @@ import (
 
 func main() {
 	lines := util.ReadLines("input.txt")
-	w := parseInput(lines)
+	marked := parseInput(lines)
 
-	fmt.Println("Part 1: ", part1(w))
+	fmt.Println("Part 1: ", part1(marked))
+
+	base := parseInput(lines)
+	fmt.Println("Part 2: ", part2(base, marked))
 }
 
 type world struct {
@@ -28,6 +31,23 @@ func (w *world) markGuardSpot() {
 	w.grid[w.guardPosition[0]][w.guardPosition[1]] = 'X'
 }
 
+var positionMarkers = map[[2]int]rune{
+	{-1, 0}: 'N',
+	{0, 1}:  'E',
+	{1, 0}:  'S',
+	{0, -1}: 'W',
+}
+
+func (w *world) markGuardSpotPart2() (onCycle bool) {
+	newMarker := positionMarkers[w.guardDirection]
+	currentMarker := w.grid[w.guardPosition[0]][w.guardPosition[1]]
+	if newMarker == currentMarker {
+		return true
+	}
+	w.grid[w.guardPosition[0]][w.guardPosition[1]] = newMarker
+	return false
+}
+
 func (w *world) advance() {
 	newRow := w.guardPosition[0] + w.guardDirection[0]
 	newCol := w.guardPosition[1] + w.guardDirection[1]
@@ -37,6 +57,20 @@ func (w *world) advance() {
 		w.markGuardSpot()
 		w.guardPosition = [2]int{newRow, newCol}
 	}
+}
+
+func (w *world) advancePart2() (onCycle bool) {
+	newRow := w.guardPosition[0] + w.guardDirection[0]
+	newCol := w.guardPosition[1] + w.guardDirection[1]
+	if w.onGrid(newRow, newCol) && w.grid[newRow][newCol] == '#' {
+		w.turnRight()
+	} else {
+		if w.markGuardSpotPart2() {
+			return true
+		}
+		w.guardPosition = [2]int{newRow, newCol}
+	}
+	return false
 }
 
 func (w *world) guardOnGrid() bool {
@@ -83,9 +117,46 @@ func parseInput(lines []string) *world {
 	return res
 }
 
+func (w *world) copy() *world {
+	newW := *w
+
+	newW.grid = make([][]rune, 0)
+	for i, row := range w.grid {
+		newW.grid = append(newW.grid, make([]rune, len(row)))
+		copy(newW.grid[i], row)
+	}
+	return &newW
+}
+
+func (w *world) cycle() bool {
+	for w.guardOnGrid() {
+		if w.advancePart2() {
+			return true
+		}
+	}
+
+	return false
+}
+
 func part1(w *world) int {
 	for w.guardOnGrid() {
 		w.advance()
 	}
 	return w.spotsVisited()
+}
+
+func part2(base, marked *world) int {
+	total := 0
+	for rowIdx, row := range marked.grid {
+		for colIdx, val := range row {
+			if val == 'X' {
+				newW := base.copy()
+				newW.grid[rowIdx][colIdx] = '#'
+				if newW.cycle() {
+					total++
+				}
+			}
+		}
+	}
+	return total
 }
